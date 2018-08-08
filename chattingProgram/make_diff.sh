af425344b2502583cd634b3e0f6b5213beeb2439
Deukhyun Park <pdh8312@yura.co.kr>
2018-08-08 15:59:33 +0900

    [Modify] add option of clean, diff for git and svn

--- chattingProgram/make_diff.sh
+++ chattingProgram/make_diff.sh
@@ -1,2 +1,14 @@
 echo "$1"
-svn log --diff -r $1 > diff_$1
+
+
+if [[ $1 == '' ]]; then
+echo "wrong command or invalid parameter"
+elif [[ $1 == 'svn' ]]; then
+svn > /dev/null && svn log --diff -r $1 > diff_$1
+elif [[ $1 == 'git' ]]; then
+git > /dev/null && git diff > diff_$1
+elif [[ $1 == 'clean' ]]; then
+rm -rf diff_*
+else
+echo "$1 is not supported or is wrong command."
+fi
