995c8a74ba60c637db3a4db91b012e1ce97167f1
Deukhyun Park <ppdha82@focushns.com>
2018-09-04 18:21:41 +0900

    [Modify] add debug mode for echo

--- mount.sh
+++ mount.sh
@@ -1,29 +1,40 @@
 #!/bin/sh
-#/sbin/udhcpc &&
-#mount -t nfs -o nolock -o tcp 192.168.0.108:/home/ppdha82/Project/007_ipm-hi/app/_output/hi3516cv300 /home &&
-#/edvr/start.sh
+
+print=echo
+#print_dbg=echo
+#$print_dbg "merong"
+
+EXECUTABLE_FILE="emount.sh"
+print "#!/bin/sh" > ${EXECUTABLE_FILE}
+print "/sbin/udhcpc &&" >> ${EXECUTABLE_FILE}
+
 ETH_DEV_eth0="eth0"
 ETH_DEV_enp3s0="enp3s0"
 IP_ADDR_DEV=`ifconfig ${ETH_DEV_enp3s0} | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}'`
-echo "IP_ADDR_DEV= $IP_ADDR_DEV"
+#print_dbg "IP_ADDR_DEV= $IP_ADDR_DEV"
 IP_ADDR=${IP_ADDR_DEV}
 
 TARGET_DIRECTORY="/home"
-SRC_DIRECTORY_KEY="_output"
+KEY_DIRECTORY="_output"
 
 MODEL=$(grep DEVELOP_MODEL app/Makefile | grep -v '\$' | grep -v '\#')
-echo ${MODEL}
-if [[ "${MODEL}" =~ "IPC-2090" || "${MODEL}" =~ "IPC-2100" ]]; then
-PLATFORM="hi3519v101"
+#print_dbg ${MODEL}
+if [[ "${MODEL}" =~ "IPC-2090" || "${MODEL}" =~ "IPC-2100" || "${MODEL}" =~ "IPC-8080" ]]; then
+	PLATFORM="hi3519v101"
 elif [[ "${MODEL}" =~ "MON-2076" ]]; then
-PLATFORM="hi3516av200"
+	PLATFORM="hi3516av200"
 else
-PLATFORM="hi3516cv300"
+	PLATFORM="hi3516cv300"
 fi
 
-directory=`find -name ${SRC_DIRECTORY_KEY}`
+directory=`find -name ${KEY_DIRECTORY}`
 cd ${directory}
 SOURCE_DIRECTORY="$PWD"
-echo "src = ${SOURCE_DIRECTORY}"
+cd -
+#print_dbg "src = ${SOURCE_DIRECTORY}"
 
-echo "mount -t nfs -o nolock -o tcp ${IP_ADDR}:${SOURCE_DIRECTORY}/${PLATFORM} ${TARGET_DIRECTORY} &&"
+
+print "mount -t nfs -o nolock -o tcp ${IP_ADDR}:${SOURCE_DIRECTORY}/${PLATFORM} ${TARGET_DIRECTORY} &&" >> ${EXECUTABLE_FILE}
+print "edvr/start.sh" >> ${EXECUTABLE_FILE}
+#print_dbg "directory = ${SOURCE_DIRECTORY}/${PLATFORM}"
+cp ${EXECUTABLE_FILE} ${SOURCE_DIRECTORY}/${PLATFORM}
