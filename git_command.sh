608209ad9063c3da6a06b78ca43337a958e4ef17
Deukhyun Park <ppdha82@gmail.com>
2019-06-10 19:05:37 +0900

    [Modify] make iteration things to function

--- git_command.sh
+++ git_command.sh
@@ -6,61 +6,49 @@
 LOG_PATH=/home/ppdha82/Downloads/git
 PROJECT_PATH=/home/ppdha82/Project
 GIT_PULL=('git pull origin master')
+GIT_PULL_ATM=('git pull origin sdk-v1.0.4.0')
 LOG_FILE=${LOG_PATH}/log.txt
-DIR_001=001_ipm-hi_for_generating_firmware
+DIR_001=001_ipm-hi_VPN_build
 DIR_002=002_ipm-hi_TTA
-DIR_004=004_ipm-hi_AF_module
+DIR_003=003_ipm-hi_VPN_build
+DIR_004=004_ipm-hi
 DIR_005=005_ipm-hi
 DIR_006=006_ipm-hi
-DIR_007=007_ipm-hi_AF2812
+DIR_007=007_ipm-hi
 DIR_008=008_ipm-hi
 DIR_009=009_ipm-hi_for_generating_firmware
-DIR_010=010_ipm-hi_AF2808
 
 DATE_LOG=`date`"\n"
 
+function git_pull() {
+	echo ${PWD} >> ${LOG_FILE}
+	${GIT_PULL} >> ${LOG_FILE}
+	echo -e ${DATE_LOG} >> ${LOG_FILE}
+}
 cd ${PROJECT_PATH}/${DIR_001}
-echo ${PWD} >> ${LOG_FILE}
-${GIT_PULL} >> ${LOG_FILE}
 echo -e ${DATE_LOG} >> ${LOG_FILE}
 
 cd ${PROJECT_PATH}/${DIR_002}
-echo ${PWD} >> ${LOG_FILE}
-${GIT_PULL} >> ${LOG_FILE}
-echo -e ${DATE_LOG} >> ${LOG_FILE}
+git_pull
+
+cd ${PROJECT_PATH}/${DIR_003}
+git_pull
 
 cd ${PROJECT_PATH}/${DIR_004}
-echo ${PWD} >> ${LOG_FILE}
-${GIT_PULL} >> ${LOG_FILE}
-echo -e ${DATE_LOG} >> ${LOG_FILE}
+git_pull
 
 cd ${PROJECT_PATH}/${DIR_005}
-echo ${PWD} >> ${LOG_FILE}
-${GIT_PULL} >> ${LOG_FILE}
-echo -e ${DATE_LOG} >> ${LOG_FILE}
+git_pull
 
 cd ${PROJECT_PATH}/${DIR_006}
-echo ${PWD} >> ${LOG_FILE}
-${GIT_PULL} >> ${LOG_FILE}
-echo -e ${DATE_LOG} >> ${LOG_FILE}
+git_pull
 
 cd ${PROJECT_PATH}/${DIR_007}
-echo ${PWD} >> ${LOG_FILE}
-${GIT_PULL} >> ${LOG_FILE}
-echo -e ${DATE_LOG} >> ${LOG_FILE}
+git_pull
 
 cd ${PROJECT_PATH}/${DIR_008}
-echo ${PWD} >> ${LOG_FILE}
-${GIT_PULL} >> ${LOG_FILE}
-echo -e ${DATE_LOG} >> ${LOG_FILE}
+git_pull
 
 cd ${PROJECT_PATH}/${DIR_009}
-echo ${PWD} >> ${LOG_FILE}
-${GIT_PULL} >> ${LOG_FILE}
-echo -e ${DATE_LOG} >> ${LOG_FILE}
-
-cd ${PROJECT_PATH}/${DIR_010}
-echo ${PWD} >> ${LOG_FILE}
-${GIT_PULL} >> ${LOG_FILE}
-echo -e ${DATE_LOG} >> ${LOG_FILE}
+git_pull
 
